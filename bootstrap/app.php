607a13cce2ad2021bcd0305632ae44c30c1b41f7
kikorobledo <enrique_j_@hotmail.com>
2024-05-21 11:13:02 -0600

    Fix acreditacion

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->validateCsrfTokens(except: [
-            'acredita_pago/*',
+            'acredita_pago',
         ]);
 
     })
