9c1f8baec58ef531b7d81f9543cb848016b0a443
root <root@DESKTOP-IVHFQKO.localdomain>
2025-07-18 11:26:07 -0600

    Fix actoescritura

--- resources/views/livewire/catastro/avisos/identificacion-inmueble-revision.blade.php
+++ resources/views/livewire/catastro/avisos/identificacion-inmueble-revision.blade.php
@@ -12,9 +12,9 @@
 
         <div class="bg-white rounded-lg p-3 flex flex-col lg:flex-row gap-3 mb-3 shadow-lg items-end">
 
-            <x-input-group for="aviso.predio.superficie_terreno" label="Superficie total de terreno (notarial)" class="w-full">
+            <x-input-group for="aviso.predio.superficie_total_terreno" label="Superficie total de terreno (notarial)" class="w-full">
 
-                <x-input-text type="number" id="aviso.predio.superficie_terreno" value="{{ $aviso->predio->superficie_total_terreno }}" readonly/>
+                <x-input-text type="number" id="aviso.predio.superficie_total_terreno" value="{{ $aviso->predio->superficie_total_terreno }}" readonly/>
 
             </x-input-group>
 
