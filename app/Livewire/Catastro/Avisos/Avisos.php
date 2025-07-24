04e05c960a8bd0dffa31fd63a16ec72d4651ca10
root <root@DESKTOP-IVHFQKO.localdomain>
2025-07-24 12:16:46 -0600

    Fix aviso

--- app/Livewire/Catastro/Avisos/Avisos.php
+++ app/Livewire/Catastro/Avisos/Avisos.php
@@ -103,6 +103,8 @@ class Avisos extends Component
 
             (new SGCService())->inactivarTraslado($this->modelo_editar->traslado_sgc);
 
+            (new PeritosExternosService())->reactivarAvaluo($this->modelo_editar->avaluo_spe);
+
             $this->modelo_editar->update([
                                             'avaluo_spe' => null,
                                             'estado' => 'nuevo',
