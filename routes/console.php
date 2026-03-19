8595b05136cb62ff3f80bf32122167c5ff905884
root <root@DESKTOP-IVHFQKO.localdomain>
2026-03-19 15:51:54 -0600

    Fix command

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('backup:clean')->daily()->at('01:00');
+Schedule::command('backup:clean')->daily()->at('15:52');
 Schedule::command('backup:run')->daily()->at('01:30');
 
 Artisan::command('inspire', function () {
