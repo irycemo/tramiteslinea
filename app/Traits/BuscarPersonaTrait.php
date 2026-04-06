b45451d59129f4be34277021dced05a6d40ad964
root <root@DESKTOP-IVHFQKO.localdomain>
2026-04-06 14:17:07 -0600

    Fix buscarpersona

--- app/Traits/BuscarPersonaTrait.php
+++ app/Traits/BuscarPersonaTrait.php
@@ -13,45 +13,22 @@ trait BuscarPersonaTrait
 
         $persona = null;
 
-        if($rfc && $curp){
-
-            $personaRfc = Persona::where('rfc', $rfc)->first();
-
-            $personaCurp = Persona::where('curp', $curp)->first();
-
-            if($personaRfc?->id != $personaCurp?->id){
-
-                throw new GeneralException('Ya esta registrada otra persona con la misma CURP o RFC');
-
-            }else{
-
-                $persona = $personaRfc;
-
-            }
-
-        }elseif($rfc){
+        if(isset($rfc)){
 
             $persona = Persona::where('rfc', $rfc)->first();
 
-        }elseif($curp){
+        }elseif(isset($curp)){
 
             $persona = Persona::where('curp', $curp)->first();
 
         }else{
 
-            if(in_array($tipo_persona, ['FISICA', 'FÍSICA'])){
-
-                $persona = Persona::query()
+            $persona = Persona::query()
                             ->where('nombre', trim($nombre))
                             ->where('ap_paterno', trim($ap_paterno))
                             ->where('ap_materno', trim($ap_materno))
+                            ->where('razon_social', trim($razon_social))
                             ->first();
-
-            }else{
-
-                $persona = Persona::where('razon_social', $razon_social)->first();
-
-            }
 
         }
 
