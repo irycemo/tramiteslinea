1c2d4a01c382ba511c263350f38c747c6de4916f
root <root@DESKTOP-IVHFQKO.localdomain>
2026-04-09 13:49:37 -0600

    Fix formateado

--- resources/views/avisos/descripcion_inmueble.blade.php
+++ resources/views/avisos/descripcion_inmueble.blade.php
@@ -2,20 +2,20 @@
 
 <p class="parrafo">
 
-    <strong>Superficie de terreno:</strong>  {{ $predio->superficie_total_terreno }} @if($predio->tipo_predio == 1) Metros cuadrado; @else Hectáreas; @endif
+    <strong>Superficie de terreno:</strong>  {{ $predio->superficie_total_terreno_formateada }} @if($predio->tipo_predio == 1) Metros cuadrado; @else Hectáreas; @endif
 
     @if ($predio->superficie_construccion)
 
-        <strong>Superficie de construcción:</strong> {{ $predio->superficie_total_construccion }} @if($predio->tipo_predio == 1) Metros cuadrados; @else Hectáreas; @endif
+        <strong>Superficie de construcción:</strong> {{ $predio->superficie_total_construccion_formateada }} @if($predio->tipo_predio == 1) Metros cuadrados; @else Hectáreas; @endif
 
     @endif
 
     @if ($predio->superficie_judicial)
-        <strong>superficie judicial:</strong>  {{ $predio->superficie_judicial }};
+        <strong>superficie judicial:</strong>  {{ $predio->superficie_judicial_formateada }};
     @endif
 
     @if ($predio->superficie_notarial)
-        <strong>superficie notarial:</strong> {{ $predio->superficie_notarial }};
+        <strong>superficie notarial:</strong> {{ $predio->superficie_notarial_formateada }};
     @endif
 
 </p>
