39fbd0b14fcd2c168209a05e3f2f55646149d856
root <root@DESKTOP-IVHFQKO.localdomain>
2026-06-16 11:13:58 -0600

    Fix buscar

--- app/Traits/ActoresTrait.php
+++ app/Traits/ActoresTrait.php
@@ -209,7 +209,7 @@ trait ActoresTrait{
         $this->validate([
             'nombre' => Rule::requiredIf($this->ap_materno || $this->ap_paterno),
             'ap_materno' => Rule::requiredIf($this->nombre || $this->ap_paterno),
-            'ap_paterno' => Rule::requiredIf($this->ap_materno || $this->nombre),
+            'ap_paterno' => 'nullable',
             'curp' => [
                 'nullable',
                 'regex:/^[A-Z]{1}[AEIOUX]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$/i'
