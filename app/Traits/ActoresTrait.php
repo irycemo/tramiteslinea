8802f8945256cbb7f178cb4ea1176df7f0028e54
root <root@DESKTOP-IVHFQKO.localdomain>
2026-06-16 11:18:39 -0600

    Fix buscar

--- app/Traits/ActoresTrait.php
+++ app/Traits/ActoresTrait.php
@@ -208,8 +208,8 @@ trait ActoresTrait{
 
         $this->validate([
             'nombre' => Rule::requiredIf($this->ap_materno || $this->ap_paterno),
-            'ap_materno' => Rule::requiredIf($this->nombre || $this->ap_paterno),
-            'ap_paterno' => 'nullable',
+            'ap_materno' => 'nullable',
+            'ap_paterno' => Rule::requiredIf($this->ap_materno || $this->nombre),
             'curp' => [
                 'nullable',
                 'regex:/^[A-Z]{1}[AEIOUX]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$/i'
