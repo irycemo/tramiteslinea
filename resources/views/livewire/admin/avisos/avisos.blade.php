c329b475e89bba8824be1934d551bb26a3f44d0f
root <root@DESKTOP-IVHFQKO.localdomain>
2026-06-19 14:37:17 -0600

    Fix audits

--- resources/views/livewire/admin/avisos/avisos.blade.php
+++ resources/views/livewire/admin/avisos/avisos.blade.php
@@ -226,6 +226,17 @@
                                         Ver rechazos
                                     </button>
 
+                                    @if(auth()->user()->hasRole('Administrador'))
+
+                                        <a
+                                            href="{{ route('auditoria') . '?modelo=Aviso&modelo_id=' . $aviso->id }}"
+                                            class="w-full text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+                                            role="menuitem">
+                                            Auditar
+                                        </a>
+
+                                    @endif
+
                                 </div>
 
                             </div>
